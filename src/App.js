fbba1b703f1de3ce04ba28515e9c945bd451b142
Kevin <khuang771@gmail.com>
2021-05-04 09:31:59 +1000

    Example of listbox code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import SelectDropdown from './SelectDropdown'
+
+function App() {
+  return (
+    <div class="flex h-screen">
+    <div class="m-auto">
+      <h1>Select Box Problem</h1>
+      <SelectDropdown/>
+    </div>
+  </div>
+  );
+}
+
+export default App;
